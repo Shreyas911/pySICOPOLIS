97bdb79a6cfaceabbd17ca91863c5866c42bbdc6
Shreyas911 <shreyasg911@gmail.com>
2024-12-16 02:12:01 -0600

    Updates to optim setup.

--- src/pySICOPOLIS/optim/optim.py
+++ src/pySICOPOLIS/optim/optim.py
@@ -260,7 +260,7 @@ def eval_gradient(sicopolis_dir,
                     raise ValueError(f"eval_gradient: {var} not present in ds_gradient!")
 
     if fields_to_ignore is not None:
-        return ds_gradient.drop_vars(fields_to_ignore)
+        return ds_gradient.drop_vars(field + "b" for field in fields_to_ignore)
     else:
         return ds_gradient
 
@@ -277,4 +277,147 @@ def subset_of_ds(ds, attr_key, attr_value):
     ds_subset = ds[subset_vars]
 
     return ds_subset
+
+def ds_compatibility_prep(list_ds, list_types,
+                          fields_to_ignore = None):
+
+    if len(list_ds) != 2 or len(list_types) != 2:
+        return ValueError("ds_compatibility_prep: Only works for two ds at a time")
+
+    list_suffixes = []
+    for type_var in list_types:
+        if type_var == "nodiff":
+            list_suffixes.append("")
+        elif type_var == "adj":
+            list_suffixes.append("b")
+        elif type_var == "tlm":
+            list_suffixes.append("d")
+        else:
+            return ValueError(f"ds_compatibility_prep: {type_var} is not a valid type for this function for now.")
+
+    ds_subset_0 = subset_of_ds(list_ds[0], "type", list_types[0]).copy()
+    ds_subset_1 = subset_of_ds(list_ds[1], "type", list_types[1]).copy()
+
+    for var_0 in ds_subset_0:
+        if list_suffixes[0] != "":
+            basic_str = var_0[:-1]
+            var_1 = basic_str + list_suffixes[1]
+        else:
+            basic_str = var_0
+            var_1 = basic_str + list_suffixes[1]
+        if var_1 not in ds_subset_1:
+            if fields_to_ignore and basic_str in fields_to_ignore:
+                pass
+            else:
+                raise ValueError(f"ds_compatibility_prep: {var_1} not present in second ds when {var_0} is present in first ds.")
+
+    for var_1 in ds_subset_1:
+        if list_suffixes[1] != "":
+            basic_str = var_1[:-1]
+            var_0 = basic_str + list_suffixes[0]
+        else:
+            basic_str = var_1
+            var_0 = basic_str + list_suffixes[0]
+        if var_0 not in ds_subset_0:
+            if fields_to_ignore and basic_str in fields_to_ignore:
+                pass
+            else:
+                raise ValueError(f"ds_compatibility_prep: {var_0} not present in second ds when {var_1} is present in first ds.")
+
+    return ds_subset_0, ds_subset_1
+
+def L2_inner_product(ds_1, ds_2, type_var, fields_to_ignore = None):
+
+    ds_subset_1, ds_subset_2 = ds_compatibility_prep([ds_1, ds_2],
+                                                      [type_var, type_var],
+                                                      fields_to_ignore = fields_to_ignore)
+
+    inner_product = 0.0
+
+    for var in ds_subset_1:
+
+        if type_var != "nodiff":
+            temp = var[:-1]
+        else:
+            temp = var
+
+        if fields_to_ignore and temp not in fields_to_ignore:
+            if ds_subset_1[var].data.shape != ds_subset_2[var].data.shape:
+                raise ValueError("L2_inner_product: {var} does not have the same shape in both datasets.")
+            inner_product = inner_product + np.sum(ds_subset_1[var].data*ds_subset_2[var].data)
+
+    return inner_product
+   
+def grad_descent_step(ds_state, ds_gradient, alpha, dict_fields_or_scalars = None, fields_to_ignore = None):
+
+    ds_s, ds_g = ds_compatibility_prep([ds_state, ds_gradient], 
+                                       ["nodiff", "adj"],
+                                       fields_to_ignore = fields_to_ignore)
+
+    for var in ds_s:
+        if fields_to_ignore and var not in fields_to_ignore:
+            if dict_fields_or_scalars is not None and var in dict_fields_or_scalars[var] == "scalar":
+                if ds_g[var + "b"].data.shape == (1,):
+                    ds_s[var].data[:] = ds_s[var].data.flat[0] - alpha*ds_g[var + "b"].data
+                else:
+                    raise ValueError(f"grad_descent_step: {var + 'b'} should be a scalar gradient value.")
+            else:
+                ds_s[var].data = ds_s[var].data - alpha*ds_g[var + "b"].data
+
+    return ds_s
+
+def line_search(ds_state, ds_gradient, ds_descent_dir,
+                eval_cost, sicopolis_dir, log_file, sico_out_folder,
+                init_alpha = 1.0,
+                ad_input_nc = "ad_input_nodiff.nc",
+                ad_output_nc = "ad_output_nodiff.nc",
+                exec_cmd = "./drivernodiff", c1 = 1.e-4, 
+                fields_to_ignore = None, dict_fields_or_scalars = None):
+
+    alpha = init_alpha
+
+    while True:
+
+        ds_s, ds_p = ds_compatibility_prep([ds_state, ds_descent_dir], 
+                                           ["nodiff", "adj"],
+                                           fields_to_ignore = fields_to_ignore)
+        _, ds_g = ds_compatibility_prep([ds_state, ds_gradient], 
+                                        ["nodiff", "adj"],
+                                        fields_to_ignore = fields_to_ignore)
+
+        fc = eval_cost(sicopolis_dir,
+                       log_file,
+                       sico_out_folder,
+                       ad_output_nc,
+                       exec_cmd)
+
+        for var in ds_s:
+            if fields_to_ignore and var not in fields_to_ignore:
+                if dict_fields_or_scalars is not None and var in dict_fields_or_scalars[var] == "scalar":
+                    if ds_p[var + "b"].data.shape == (1,):
+                        ds_s[var].data[:] = ds_s[var].data.flat[0] + alpha*ds_p[var + "b"].data
+                    else:
+                        raise ValueError(f"line_search: {var + 'b'} should be a scalar gradient value.")
+                else:
+                    ds_s[var].data = ds_s[var].data + alpha*ds_p[var + "b"].data
+
+        ad_io_dir = sicopolis_dir + "/src/subroutines/tapenade/ad_io"
+        ds_s.to_netcdf(ad_io_dir + "/" + ad_input_nc)
+
+        fc_new = eval_cost(sicopolis_dir,
+                           log_file,
+                           sico_out_folder,
+                           ad_output_nc,
+                           exec_cmd)
+
+        pTg = L2_inner_product(ds_p, ds_g, type_var="adj", fields_to_ignore=fields_to_ignore)
+
+        ratio = (fc_new - fc)/(alpha*pTg)
+
+        print(ratio)
+        if ratio >= c1:
+            print(f"alpha = {alpha}")
+            return alpha
+
+        alpha = alpha/2.0
 
