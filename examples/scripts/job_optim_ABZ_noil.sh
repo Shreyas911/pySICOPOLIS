b596456836b48c6b63f7b475014c3bf3b82ef235
Shreyas911 <shreyasg911@gmail.com>
2025-09-17 03:47:12 -0500

    New scripts

---
+++ examples/scripts/job_optim_ABZ_noil.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=oA_nil
+#SBATCH --output=oA_nil_%j.out
+#SBATCH --error=oA_nil_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=2400:00:00
+
+srun --exclusive -N1 -n1 python -u optim_ABZ_noil.py
+
